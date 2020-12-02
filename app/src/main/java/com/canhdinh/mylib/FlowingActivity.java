0e982ddaf1ddbb1dd5ba595e7ebea816c18b77de
dinhdeveloper <dinhtrancntt@gmail.com>
2020-12-02 14:09:05 +0700

    add

---
+++ app/src/main/java/com/canhdinh/mylib/FlowingActivity.java
@@ -0,0 +1,79 @@
+package com.canhdinh.mylib;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.canhdinh.lib.flowingdrawer_core.ElasticDrawer;
+import com.canhdinh.lib.flowingdrawer_core.FlowingDrawer;
+
+public class FlowingActivity extends AppCompatActivity {
+    private RecyclerView rvFeed;
+    private FlowingDrawer mDrawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flowing);
+
+        rvFeed = (RecyclerView) findViewById(R.id.rvFeed);
+        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
+
+        rvFeed = (RecyclerView) findViewById(R.id.rvFeed);
+        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+        setupToolbar();
+        setupMenu();
+    }
+
+    protected void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawer.toggleMenu();
+            }
+        });
+    }
+
+
+    private void setupMenu() {
+        FragmentManager fm = getSupportFragmentManager();
+        MainFragment mMenuFragment = (MainFragment) fm.findFragmentById(R.id.id_container_menu);
+        if (mMenuFragment == null) {
+            mMenuFragment = new MainFragment();
+            fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment).commit();
+        }
+
+//        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+//            @Override
+//            public void onDrawerStateChange(int oldState, int newState) {
+//                if (newState == ElasticDrawer.STATE_CLOSED) {
+//                    Log.i("MainActivity", "Drawer STATE_CLOSED");
+//                }
+//            }
+//
+//            @Override
+//            public void onDrawerSlide(float openRatio, int offsetPixels) {
+//                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
+//            }
+//        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isMenuVisible()) {
+            mDrawer.closeMenu();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
